33f93995c6ef49302dfe0c66ffbf8b99a09ef672
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-05-25 01:51:27 -0400

    contact wip

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
-	import ProjectTile from "$lib/components/ProjectTile.svelte";
+	import { enhance } from '$app/forms';
+	export let form;
+	let name: string = '';
+	let email: string = '';
+	let title: string = '';
+	let message: string = '';
 </script>
 
 <svelte:head>
-    <title>Contact - Ethan McIntyre</title>
+	<title>Contact - Ethan McIntyre</title>
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline">Contact world!</h1>
-
-
-
+<div class="w-2/3 mx-auto pt-6">
+    <h2 class="mb-4 mr-6 font-bold text-3xl text-center">Contact</h2>
+    <form method="post" class="flex flex-col" action="?/submit" use:enhance>
+        <div class="grid grid-cols-2 w-full gap-4 mb-8">
+            <div class="flex flex-col md:col-span-1 col-span-2">
+                <label for="name" placeholder="Your Name" class="font-bold">Name:</label>
+                <input
+                    name="name"
+                    id="name"
+                    type="text"
+                    class={`${form?.errors?.name ? 'border-red-700' : ''} border-2 p-2 rounded-sm border-stone-700 hover:border-indigo-700 focus:border-indigo-700 bg-white transition-all`}
+                    bind:value={name}
+                />
+            </div>
+            <div class="flex flex-col md:col-span-1 col-span-2">
+                <label for="email" placeholder="your.email@email.net" class="font-bold">Email:</label>
+                <input
+                    name="email"
+                    id="email"
+                    type="email"
+                    class={`${form?.errors?.email ? 'border-red-700' : ''} border-2 p-2 rounded-sm border-stone-700 hover:border-indigo-700 focus:border-indigo-700 bg-white transition-all`}
+                    bind:value={name}
+                />
+            </div>
+        </div>
+    </form>
+</div>
