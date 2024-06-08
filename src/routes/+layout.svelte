d42671b9f3726b818aa6fb449993ae2bfb1c4bab
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-06-08 16:54:49 -0400

    new images, some edits to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import { page } from '$app/stores';
 	import { fade } from 'svelte/transition';
 	// @ts-ignore
 	import MenuIcon from 'virtual:icons/mdi/menu';
@@ -9,39 +10,54 @@
 		console.log('opened')
 	}
 </script>
+<div class="h-full flex flex-col">
+	<nav class="flex grow-0 flex-wrap items-center px-4 md:px-32 py-2 md:py-4 lg:py-6 border-b-2 bg-white gap-4 w-full">
+		<a href="/" class=""><h1 class="text-2xl md:text-4xl italic font-bold font-deco hover:text-indigo-700 transition-all ease-in-out">Ethan McIntyre</h1></a>
+		<div class="hidden md:block h-1 w-16 mx-0 border-0 bg-black">&nbsp;</div>
+		<a
+			aria-current={$page.url.pathname === '/'}
+			href="/"
+			class={`
+				${$page.url.pathname === '/' ? 'text-orange-500 font-semibold underline-offset-4' : 'underline-offset-2'}
+				hidden md:block text-xl text-indigo-700 underline hover:underline-offset-8 transition-all ease-in-out`
+			}
+			>home</a
+		>
+		<a
+			aria-current={$page.url.pathname === '/projects'}
+			href="/projects"
+			class={`
+				${$page.url.pathname === '/projects' ? 'text-orange-500 font-semibold underline-offset-4' : 'underline-offset-2'}
+				hidden md:block text-xl text-indigo-700 underline hover:underline-offset-8 transition-all ease-in-out`
+			}
+			>projects</a
+		>
+		<a
+			aria-current={$page.url.pathname === '/contact'}
+			href="/contact"
+			class={`
+				${$page.url.pathname === '/contact' ? 'text-orange-500 font-semibold underline-offset-4' : 'underline-offset-2'}
+				hidden md:block text-xl text-indigo-700 underline hover:underline-offset-8 transition-all ease-in-out`
+			}
+			>contact</a
+		>
+		<button><MenuIcon class="block md:hidden text-indigo-700" style="font-size:1.5em"/></button>
+	</nav>
 
-<nav class="flex flex-wrap items-center px-4 md:px-32 py-2 border-b-2 bg-white gap-4 w-full">
-	<a href="/" class=""><h1 class="text-2xl md:text-5xl italic font-bold font-deco hover:text-indigo-700 transition-all ease-in-out">Ethan McIntyre</h1></a>
-	<div class="hidden md:block h-1 w-16 mx-0 border-0 bg-black">&nbsp;</div>
-	<a
-		href="/"
-		class="hidden md:block text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
-		>home</a
-	>
-	<a
-		href="/projects"
-		class="hidden md:block  text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
-		>projects</a
-	>
-	<a
-		href="/contact"
-		class="hidden md:block text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
-		>contact</a
-	>
-	<button><MenuIcon class="block md:hidden text-indigo-700" style="font-size:1.5em"/></button>
-</nav>
 
+	<!-- todo: tweak/customize this? -->
+	{#key data.pathname}
+		<div
+			id="main-container"
+			class="flex grow w-full overflow-scroll"
+			in:fade={{ duration: 300, delay: 400 }}
+			out:fade={{ duration: 300 }}
+		>
+			<slot />
+		</div>
+	{/key}
 
-<!-- todo: tweak/customize this? -->
-{#key data.pathname}
-	<div
-		class="px-16 md:px-32 to-indigo-50 min-h-fit w-full"
-		in:fade={{ duration: 300, delay: 400 }}
-		out:fade={{ duration: 300 }}
-	>
-		<slot />
-	</div>
-{/key}
+</div>
 
 <!-- Footer idea for now 
 <footer class="flex items-center px-14 py-6 border-b-2 bg-white gap-4"></footer>
@@ -52,8 +68,9 @@
 		background-color: theme(colors.neutral.300);
 	}
 
-	:global(body) {
-		min-height: 100%;
+	:global(body, html) {
+		height: 100%;
+  		margin: 0;
 		width: 100%;
 		position: relative;
 	}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import ProjectTile from "$lib/components/ProjectTile.svelte";
+    import NameType from "$lib/assets/name_stacked.png"
+    import EthanImg from "$lib/assets/photo_of_me.jpg"
 </script>
 
 <svelte:head>
     <title>Home - Ethan McIntyre</title>
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<div class="h-full grid grid-cols-3 overflow-hidden">
+    <div class="col-span-1 overflow-hidden relative">
+        <img class="object-cover h-full" src={EthanImg} alt="A photo of Ethan McIntyre"/>
+    </div>
+<div class="col-span-2">
+    <p id="intro-text">Hi My Name is Ethan!!!</p>
+</div>
+</div>
