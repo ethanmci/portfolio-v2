d809969c6057eddb3a7d2c345e2a16a05d1d7789
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-06-08 13:46:39 -0400

    quicktime to not embarass myself

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,41 @@
 <script>
 	import '../app.css';
 	import { fade } from 'svelte/transition';
+	// @ts-ignore
+	import MenuIcon from 'virtual:icons/mdi/menu';
 	export let data;
+
+	function OpenMenu() {
+		console.log('opened')
+	}
 </script>
 
-<nav class="flex items-center px-16 md:px-32 py-6 border-b-2 bg-white gap-4 w-full">
-	<a href="/"><h1 class="text-5xl italic font-bold font-deco hover:text-indigo-700 transition-all ease-in-out">Ethan McIntyre</h1></a>
-	<div class="h-1 w-16 mx-0 border-0 bg-black rounded-sm">&nbsp;</div>
+<nav class="flex flex-wrap items-center px-4 md:px-32 py-2 border-b-2 bg-white gap-4 w-full">
+	<a href="/" class=""><h1 class="text-2xl md:text-5xl italic font-bold font-deco hover:text-indigo-700 transition-all ease-in-out">Ethan McIntyre</h1></a>
+	<div class="hidden md:block h-1 w-16 mx-0 border-0 bg-black">&nbsp;</div>
 	<a
 		href="/"
-		class="text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
+		class="hidden md:block text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
 		>home</a
 	>
 	<a
 		href="/projects"
-		class="text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
+		class="hidden md:block  text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
 		>projects</a
 	>
 	<a
 		href="/contact"
-		class="text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
+		class="hidden md:block text-2xl text-indigo-700 underline underline-offset-2 hover:underline-offset-4 transition-all ease-in-out"
 		>contact</a
 	>
+	<button><MenuIcon class="block md:hidden text-indigo-700" style="font-size:1.5em"/></button>
 </nav>
 
 
 <!-- todo: tweak/customize this? -->
 {#key data.pathname}
 	<div
-		class="px-14 bg-gradient-to-b from-neutral-300 to-indigo-50 min-h-fit w-full"
+		class="px-16 md:px-32 to-indigo-50 min-h-fit w-full"
 		in:fade={{ duration: 300, delay: 400 }}
 		out:fade={{ duration: 300 }}
 	>
