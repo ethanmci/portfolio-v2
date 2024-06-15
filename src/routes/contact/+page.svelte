79eb2141391264014d66ec217d759ffb8d9aeb22
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2024-06-15 03:26:18 -0400

    some small fixes for contact

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { fade } from 'svelte/transition'
+	import { fade, fly } from 'svelte/transition'
 	import type { ActionData } from './$types';
+	import ForwardArrow from 'virtual:icons/ic/baseline-arrow-forward';
+	import { quartInOut } from 'svelte/easing';
 	// initializing all varables at once
 	let name: string = '',
 		email: string = '',
@@ -18,8 +20,7 @@
 	$: emailError = !emailReg.test(email);
 	$: messageError = message.length <= 0;
 
-	let truthTest = (e: boolean) => {return e};
-	$: submitted = [messageError, emailError, nameError].some(truthTest);
+	let truthTest = (e: boolean) => {return e}
 
 	export let form: ActionData;
 </script>
@@ -29,8 +30,15 @@
 </svelte:head>
 
 {#if form?.success}
-	<div class="w-2/3 mx-auto pt-6">
-		<p class="text-center text-xl">Message sent, thank you!</p>
+	<div class="w-1/3 mx-auto pt-6">
+		<p class="font-bold text-xl">Message sent, thank you!</p>
+		<a href="/" class="block mx-auto">
+			<button
+				transition:fly={{ duration: 1000, y: 300, opacity: 0.0, easing: quartInOut }}
+				class="flex font-deco font-bold my-auto p-2 text-stone-50 bg-indigo-700 hover:bg-indigo-900 hover:shadow-sm rounded-sm transition-all ease-in-out"
+				>Return home<ForwardArrow class="my-auto ml-2" /></button
+			>
+		</a>
 	</div>
 {:else}
 <div class="w-2/3 mx-auto pt-6">
@@ -82,10 +90,10 @@
 			bind:value={message}
 		/>
 		<input
-			class="flex font-deco font-bold p-2 w-1/3 text-stone-50 bg-indigo-700 hover:bg-indigo-900 hover:shadow-sm rounded-sm transition-all ease-in-out"
+			class="flex font-deco font-bold p-2 w-1/3 text-stone-50 bg-indigo-700 hover:bg-indigo-900 disabled:bg-slate-800 hover:shadow-sm rounded-sm transition-all ease-in-out"
 			value="Submit"
 			type="submit"
-			disabled={ableToSubmit}
+			disabled={nameError || emailError || messageError}
 		/>
 	</form>
 </div>
