202d772f9f64f9aee369a027cc40743d2060db30
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2025-04-05 15:49:45 -0400

    formatting and hover outline colour

--- src/lib/components/ImageTiles.svelte
+++ src/lib/components/ImageTiles.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	export let cover: string;
 	export let images: string[];
-    export let selectedImage: SelectedImage;
-    
-    const openImage = (image: string) => {
-        selectedImage.image = image
-        selectedImage.isSelected = true;
-    }
+	export let selectedImage: SelectedImage;
+
+	const openImage = (image: string) => {
+		selectedImage.image = image;
+		selectedImage.isSelected = true;
+	};
 </script>
 
 <!-- svelte-ignore a11y-img-redundant-alt -->
@@ -14,13 +14,16 @@
 {#if images.length > 0}
 	<div class="grid grid-cols-3 gap-2 mt-2">
 		{#each images as image, i}
-			<button on:click={() => openImage(image)} class={`col-span-${i % 3 === 0 ? '1' : '2'} group-hover:shadow-lg`}>
+			<button
+				on:click={() => openImage(image)}
+				class={`col-span-${i % 3 === 0 ? '1' : '2'} group-hover:shadow-lg`}
+			>
 				<!-- svelte-ignore a11y-missing-attribute -->
-				<img 
-					class="border-2 border-stone-50 object-cover w-full h-full cursor-pointer hover:opacity-80 hover:hue-rotate-50"
+				<img
+					class="border-2 border-stone-50 object-cover w-full h-full cursor-pointer hover:opacity-80 hover:hue-rotate-50 hover:border-indigo-500"
 					src={image}
 				/>
-            </button>
+			</button>
 		{/each}
 	</div>
 {/if}
