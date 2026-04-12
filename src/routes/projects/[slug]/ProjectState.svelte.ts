3e0519bbe0f4536e20ffbd1da051b369af0114c0
Ethan McIntyre <ethan.sndr.mc@gmail.com>
2026-04-12 13:54:23 -0400

    project state class

---
+++ src/routes/projects/[slug]/ProjectState.svelte.ts
@@ -0,0 +1,27 @@
+import { urlFor } from "$lib/sanity";
+
+export class ProjectState {
+	#expandedImage: string | undefined = $state(undefined);
+  #projectModal: HTMLDialogElement | undefined = $state(undefined);
+
+  get expandedImage(): string | undefined {
+    return this.#expandedImage;
+  }
+
+  set projectModal(modal: HTMLDialogElement | undefined) {
+    if (!modal) return;
+    this.#projectModal = modal;
+  }
+
+  set expandedImage(imageUrl: string) {
+    if (this.#projectModal === undefined) return;
+    this.#expandedImage = urlFor(imageUrl).format("webp").quality(100).url();
+    this.#projectModal.showModal();
+  }
+
+  closeImage = () => {
+    if(!this.#projectModal?.open) return
+    this.#projectModal?.close()
+  }
+
+}
